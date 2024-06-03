f2c9aded662df78f8a26ffea43d6d08620ff9011
Moiz Mughal <moizmughal@outlook.com>
2024-06-03 00:53:53 -0700

    2nd part

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,6 @@ import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [listItems, setListItems] = useState([
-    "Gaming",
-    "Hiking",
-    "Biking",
-  ]);
-
-  const handleReorder = () => {
-    setListItems([...listItems].reverse());
-  };
-
   return (
     <>
       <Head>
@@ -29,61 +19,49 @@ export default function Home() {
         <ul>
           <li><a href="#">Home</a></li>
           <li><a href="#">Things to Read</a></li>
+          <li><a href="#">My Intrest</a></li>
+
         </ul>
       </nav>
       <main className={`${styles.main} ${inter.className}`}>
         <div>
           <h1>
-            Welcome to my personal website!
+            Why I love Biking!
           </h1>
           <br />
           <p>
-            My name is Moiz Mughal and this is my website that I built using <code>React</code> and <code> NextJS</code>.
+            I love Biking because it's a good workout and it's exciting!
             <br />
             <br />
-            I'm a 22 your old developer based in <code>Seattle, Washington</code>!
-            <br />
-            <br />
-            Some of the things i'm interested in are:
+            It's a fun way to get around that's supported by pretty much everyone.
           </p>
-          <br />
-          <ul>
-            {listItems.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul>
-          <br />
-          <br />
-          <button onClick={handleReorder}>
-            Reorder list?
-          </button>
 
           <br />
           <br />
           <div className={styles.imageContainer}>
             <Image
-              src="/hiking.webp"
+              src="/Biking1.jpeg"
               alt="Hiking"
               width={300}
               height={200}
             />
           </div>
-
-          <h2>
-            Things to read:
-          </h2>
-          <ul>
-            <li><a href="https://www.spiceworks.com/tech/devops/articles/what-is-dynamic-programming/">What is Dynamic Programming? Working, Algorithms, and Examples</a></li>
-            <li><a href="https://www.spiceworks.com/tech/devops/guest-article/high-availability-for-sql-server-in-containers/">Ensuring AI Workload HA: SQL Server in K8s Made Reliable</a></li>
-            <li><a href="https://www.spiceworks.com/tech/it-strategy/articles/linear-programming/">What Is Linear Programming? Meaning, Methods, and Examples</a></li>
-            <li><a href="https://www.spiceworks.com/tech/devops/guest-article/how-to-ease-the-pain-points-of-managing-open-source-software/">How to Ease the Pain Points of Managing Open-Source Software</a></li>
-            <li><a href="https://www.spiceworks.com/tech/it-careers-skills/articles/computer-programmer-job-role/">Computer Programmer: Job Description, Key Skills, and Salary in 2023</a></li>
-            <li><a href="https://www.spiceworks.com/tech/devops/guest-article/software-development-challenges/">Top Software Development Challenges in 2023</a></li>
-            <li><a href="https://www.spiceworks.com/tech/tech-general/articles/what-is-assembly-language/">What Is Assembly Language? Working, Features, and Advantages</a></li>
-            <li><a href="https://www.spiceworks.com/tech/devops/guest-article/why-developer-experience-needs-to-be-a-company-wide-okr/">Why Developer Experience Needs To Be a Company-wide OKR</a></li>
-            <li><a href="https://www.spiceworks.com/tech/tech-general/articles/machine-vs-assembly-language/">Machine Language vs. Assembly Language: Key Differences</a></li>
-            <li><a href="https://www.spiceworks.com/tech/devops/articles/software-developer-vs-software-engineer/">Software Developer vs. Software Engineer: Top 10 Differences</a></li>
-          </ul>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/Biking2.jpg"
+              alt="Hiking"
+              width={300}
+              height={200}
+            />
+          </div>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/Biking3.jpg"
+              alt="Hiking"
+              width={300}
+              height={200}
+            />
+          </div>
         </div>
       </main>
     </>
