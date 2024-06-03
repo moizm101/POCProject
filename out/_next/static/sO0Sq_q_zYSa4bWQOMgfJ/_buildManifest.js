c229824f38d9eabb3051f885de462981592f39cc
Moiz Mughal <moizmughal@outlook.com>
2024-06-03 00:42:11 -0700

    Main proj files (technically)

---
+++ out/_next/static/sO0Sq_q_zYSa4bWQOMgfJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/914c1da54b8f41db.css","static/chunks/pages/index-627ae24088357917.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
